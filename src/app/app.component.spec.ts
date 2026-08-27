39118f3e8059b2a516a5e1fd07e838b4cdfbe871
Santiago Ospina <santiago.ospina11@correo.tdea.edu.co>
2026-08-27 12:03:55 -0500

    refactor:test organizados

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ang-gestion-eventos');
   });
 
-  it('should render title', () => {
+  it('should render the router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('ang-gestion-eventos');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
