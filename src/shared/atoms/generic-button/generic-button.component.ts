40671f9b65422c2a571c534ebb2089aa3ef3ff61
Santiago Ospina <santiago.ospina11@correo.tdea.edu.co>
2026-09-06 13:54:22 -0500

    feat

--- src/shared/atoms/generic-button/generic-button.component.ts
+++ src/shared/atoms/generic-button/generic-button.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange } from '@angular/core';
 @Component({
   selector: 'app-generic-button',
   imports: [CommonModule],
   templateUrl: './generic-button.component.html',
   styleUrl: './generic-button.component.css'
 })
-export class GenericButtonComponent {
+export class GenericButtonComponent implements OnChanges {
   @Input() label: string = 'Ingresar';
   @Input() type: string = 'submit';
   @Input() customClass: string = 'w-full ';
   @Input() disabled: boolean = false;
   @Input() onClick!: () => void;
   @Input() customStyle: { [key: string]: string } = {};
+
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }): void {
+    if (changes['disabled']) {
+      const valorActual = changes['disabled'].currentValue;
+      if (valorActual === true) {
+        this.customClass = this.customClass.replace(' cursor-pointer', '');
+      } else {
+        if (!this.customClass.includes('cursor-pointer')) {
+          this.customClass += ' cursor-pointer';
+        }
+      }
+    }
+  }
 
   ejecutarClick() {
     if (this.onClick) {
